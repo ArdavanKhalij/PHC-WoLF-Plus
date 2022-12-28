2f1ca255baa44ef0a0fc9694bf04e0a0a5b654ef
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-12-28 18:10:55 +0100

    Update Matching Pennies Game.py

--- Ardavan/Matching Pennies Game.py
+++ Ardavan/Matching Pennies Game.py
@@ -4,3 +4,17 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+
+
+################################################################################
+# Games running data
+np.set_printoptions(threshold=np.inf)
+
+total_states = 8
+total_actions = 3
+iterations = 15000
+alpha = 0.2   #Learning Rate
+df = 0.8  #discount Factor
+d_win = 0.0025
+d_lose = 0.01
+end = total_actions - 1
