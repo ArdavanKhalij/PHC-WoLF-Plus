3f6c1b4a509cc39dcf24b7e0b42562f06176f9f8
NicolasRow <93381050+NicolasRow@users.noreply.github.com>
2023-01-05 13:50:41 +0100

    Adding the other barplots and changing the order

--- SoccerGame/barplot.py
+++ SoccerGame/barplot.py
@@ -2,18 +2,31 @@ import matplotlib.pyplot as plt
 
 # RESULTS
 # -------------------------------------------
+# MINMAX-Q:
+# Winning possibility: 0.375
+# Standard Deviation: 0
+# -------------------------------------------
 # PHC-WOLF:
 # Winning possibility: 0.43301999999999996
 # Standard Deviation: 0.03754819835890931
 # -------------------------------------------
-# Ardavan — Yesterday at 11:11 PM
 # PHC-WOLF 2X:
 # Winning possibility: 0.45670700000000003
 # Standard Deviation: 0.0357809877625417
 # -------------------------------------------
-# MINMAX-Q:
-# Winning possibility: 0.375
-# Standard Deviation: 0
+# PHC-LOSE:
+# Winning possibility:
+# Standard Deviation:
+# -------------------------------------------
+# PHC-WIN:
+# Winning possibility:
+# Standard Deviation:
+# -------------------------------------------
+
+MINMAX_Q_label = "MINMAX Q"
+MINMAX_Q_winning_probability = 37.5
+MINMAX_Q_standard_deviation = 0.1 # Just to show a black dot on the graph
+MINMAX_Q_color = "gray"
 
 PHC_WOLFx1_label = "PHC WOLF 1x"
 PHC_WOLFx1_winning_probability = 43.301999999999996
@@ -25,15 +38,20 @@ PHC_WOLFx2_winning_probability = 45.670700000000003
 PHC_WOLFx2_standard_deviation = 3.57809877625417
 PHC_WOLFx2_color = "gray"
 
-MINMAX_Q_label = "MINMAX Q"
-MINMAX_Q_winning_probability = 37.5
-MINMAX_Q_standard_deviation = 0.1 # Just to show a black dot on the graph
-MINMAX_Q_color = "gray"
+PHC_LOSE_label = "PHC(L)"
+PHC_LOSE_winning_probability = 25.20658212283894
+PHC_LOSE_standard_deviation = 2.300295587860784
+PHC_LOSE_color = "gray"
 
-probabilities = [PHC_WOLFx1_winning_probability, PHC_WOLFx2_winning_probability, MINMAX_Q_winning_probability]
-stds = [PHC_WOLFx1_standard_deviation, PHC_WOLFx2_standard_deviation, MINMAX_Q_standard_deviation]
-labels = [PHC_WOLFx1_label, PHC_WOLFx2_label, MINMAX_Q_label]
-colors = [PHC_WOLFx1_color, PHC_WOLFx2_color, MINMAX_Q_color]
+PHC_WIN_label = "PHC(W)"
+PHC_WIN_winning_probability = 34.110079400467563
+PHC_WIN_standard_deviation = 0.4639794883974892
+PHC_WIN_color = "gray"
+
+probabilities = [MINMAX_Q_winning_probability, PHC_WOLFx1_winning_probability, PHC_WOLFx2_winning_probability, PHC_LOSE_winning_probability, PHC_WIN_winning_probability]
+stds = [MINMAX_Q_standard_deviation, PHC_WOLFx1_standard_deviation, PHC_WOLFx2_standard_deviation, PHC_LOSE_standard_deviation, PHC_WIN_standard_deviation]
+labels = [MINMAX_Q_label, PHC_WOLFx1_label, PHC_WOLFx2_label, PHC_LOSE_label, PHC_WIN_label]
+colors = [MINMAX_Q_color, PHC_WOLFx1_color, PHC_WOLFx2_color, PHC_LOSE_color, PHC_WIN_color]
 
 fig, ax = plt.subplots()
 ax.axhline(y=10, linestyle=':', color='gray')
